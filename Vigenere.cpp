660483495f7a48e7496e032176fb592c99256e10
cnguyen115 <cnguyen115@gmail.com>
2014-03-11 02:31:37 -0700

    Vigenere - First Version
    Incorrect Formula

---
+++ Vigenere.cpp
@@ -0,0 +1,89 @@
+#include "Vigenere.h"
+#include <stdlib.h>	
+
+/**
+ * Sets the key to use
+ * @param key - the key to use
+ * @return - True if the key is valid and False otherwise
+ */
+bool Vigenere::setKey(const string& mykey)
+{ 	// validate the key, check if its empty
+	if ((!mykey.empty()))
+	{
+		key = mykey;
+		return true;
+	}
+	else
+		return false;
+}
+
+
+
+
+/**	
+ * Encrypts a plaintext string
+ * @param plaintext - the plaintext string
+ * @return - the encrypted ciphertext string
+ */	
+string Vigenere::encrypt(const string& plaintext)
+{ 
+	string ciphertext = "";
+		
+	// Make the key and plaintext string to all lowercase
+	string lowerkey, lowerptext;
+	
+	for (unsigned int i = 0; i < key.size(); i++)
+	{
+		lowerkey.push_back(tolower(key.at(i)));
+	}
+	
+	for (unsigned int i = 0; i < plaintext.size(); i++)
+	{
+		lowerptext.push_back(tolower(plaintext.at(i)));
+	}
+	
+	char base; // This is the starting base ASCII for each char of the key
+	
+	// Iterate through each char of the key, set each one as base and use that to encrypt
+	for (unsigned int i = 0; i < lowerptext.size(); i++)
+	{
+		base = lowerkey.at(i % lowerkey.size()); // Use modulus to loop through the key string
+		ciphertext.push_back((base - 'a' + lowerptext.at(i)) % 26 + 'a');
+	}
+	
+	return ciphertext; 
+}
+
+/**
+ * Decrypts a string of ciphertext
+ * @param cipherText - the ciphertext
+ * @return - the plaintext
+ */
+string Vigenere::decrypt(const string& cipherText) 
+{ 
+	string plaintext = "";
+	
+	// Make the key and plaintext string to all lowercase
+	string lowerkey, lowerctext;
+	
+	for (unsigned int i = 0; i < key.size(); i++)
+	{
+		lowerkey.push_back(tolower(key.at(i)));
+	}
+	
+	for (unsigned int i = 0; i < cipherText.size(); i++)
+	{
+		lowerctext.push_back(tolower(cipherText.at(i)));
+	}
+	
+	char base; // This is the starting base ASCII for each char of the key
+	
+	// Iterate through each char of the key, set each one as base and use that to encrypt
+	for (unsigned int i = 0; i < lowerctext.size(); i++)
+	{
+		base = key.at(i % key.size()); // Use modulus to loop through the key string
+		plaintext.push_back((base - 'a' + (26 - lowerctext.at(i))) % 26 + 'a');
+	}
+	
+	return plaintext; 
+}
