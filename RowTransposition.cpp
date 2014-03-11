bcae45a0215f486a8c12d1443d672735b4e88136
kevincheng99 <kevincheng99@csu.fullerton.edu>
2014-03-11 05:48:19 -0700

    initiate Row Transposition Cipher

---
+++ RowTransposition.cpp
@@ -0,0 +1,40 @@
+#include "RowTransposition.h"
+
+
+/**
+ * Sets the key to use
+ * @param key - the key to use
+ * @return - True if the key is valid and False otherwise
+ */
+bool RowTransposition::setKey(const string& key)
+{ 
+	
+	return false;  
+}
+
+
+
+
+/**	
+ * Encrypts a plaintext string
+ * @param plaintext - the plaintext string
+ * @return - the encrypted ciphertext string
+ */
+string RowTransposition::encrypt(const string& plaintext)
+{ 
+	
+	
+	return ""; 
+}
+
+/**
+ * Decrypts a string of ciphertext
+ * @param cipherText - the ciphertext
+ * @return - the plaintext
+ */
+string RowTransposition::decrypt(const string& cipherText) 
+{ 
+	return ""; 
+	
+}
+
