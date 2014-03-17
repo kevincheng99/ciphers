2a5046d88dc2e9a39668ba2a928215c49119cefb
cnguyen115 <cnguyen115@gmail.com>
2014-03-17 02:10:01 -0700

    Railfence Completed

--- Railfence.cpp
+++ Railfence.cpp
@@ -1,5 +1,7 @@
 #include "Railfence.h"
 
+#include <iostream>
+
 /**
  * Sets the key to use
  * @param key - the key to use
@@ -27,7 +29,18 @@ bool Railfence::setKey(const string& mykey)
 string Railfence::encrypt(const string& plaintext)
 { 
 	string ciphertext = "";
-		
+	
+	vector<string> ctext(key, ""); // To contain the letters in the string for Railfence
+	
+	for (unsigned int i = 0; i < plaintext.size(); i++)
+	{
+		ctext.at(i % key).push_back(plaintext.at(i)); // Repeat and loop through each vector string and pushback a character
+	}
+	
+	for (vector<string>::iterator it = ctext.begin(); it != ctext.end(); ++it)
+	{
+		ciphertext.append(*it); // Concatenate all the strings in order
+	}
 	
 	return ciphertext; 
 }
@@ -41,6 +54,34 @@ string Railfence::decrypt(const string& cipherText)
 { 
 	string plaintext = "";
 	
+	vector<string> ptext(key, ""); // To contain the letters in the string for Railfence
+	
+	int textlength = cipherText.size() / key; // Store length of the string that is split by the size of the key
+	int remainder = cipherText.size() % key; // Used to fill the remaining characters onto the first row
+	int stringcount = 0;
+	
+	ptext.at(0).append(cipherText.substr(0,textlength + remainder)); // Append the first row with the remainder
+	stringcount = textlength + remainder;
+	
+	
+	for (unsigned int i = 1; i < key; i++)
+	{
+		ptext.at(i).append(cipherText.substr(stringcount,textlength)); // Append each vector row with the string split by key (textlength)
+		stringcount += textlength;
+	}
+	
+	
+	for (unsigned int i = 0; i <= textlength; i++)
+	{
+		for (unsigned int j = 0; j < key; j++)
+		{
+			if (!ptext.at(j).empty())
+			{
+				plaintext.push_back(ptext.at(j).at(0));
+				ptext.at(j).erase(0,1);
+			}
+		}
+	}
 	
 	return plaintext; 
 }
