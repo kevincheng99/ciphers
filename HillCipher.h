2058187a6366565fbb7738d3b5b088ffd44d3cf4
cnguyen115 <cnguyen115@gmail.com>
2014-03-17 02:27:33 -0700

    Hill Cipher - Start

---
+++ HillCipher.h
@@ -0,0 +1,49 @@
+#ifndef HILLCIPHER_H
+#define HILLCIPHER_H
+
+#include <string>   /* For C++ strings */
+#include <stdio.h>  /* For standard I/O */
+#include <stdlib.h> /* For miscellenous C functions */
+#include "CipherInterface.h"
+
+using namespace std;
+
+/**
+ * This class implements the caesar cipher.
+ * The class extends the abstract class 
+ * CipherInterface.
+ */
+
+class HillCipher: public CipherInterface
+{
+	/** The public members **/
+	public:
+
+		/**
+		 * Sets the key to use
+		 * @param key - the key to use
+		 * @return - True if the key is valid and False otherwise
+		 */
+		virtual bool setKey(const string& key);
+
+		/**	
+		 * Encrypts a plaintext string
+		 * @param plaintext - the plaintext string
+		 * @return - the encrypted ciphertext string
+		 */
+		virtual string encrypt(const string& plaintext);
+
+		/**
+		 * Decrypts a string of ciphertext
+		 * @param ciphertext - the ciphertext
+		 * @return - the plaintext
+		 */
+		virtual string decrypt(const string& ciphertext);
+		
+		/* The protected members */
+	protected:
+		string key;
+
+};
+
+#endif
