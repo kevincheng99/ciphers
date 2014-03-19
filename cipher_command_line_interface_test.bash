1da20d04055093ac420f812103aae0c8688e52b1
kevincheng99 <kevincheng99@csu.fullerton.edu>
2014-03-19 02:27:22 -0700

    update cipher_command_line_interface_test.bash

--- cipher_command_line_interface_test.bash
+++ cipher_command_line_interface_test.bash
@@ -1,47 +1,143 @@
 #!/bin/bash
 
+# make the executables FRESH
+make clean
+make
+
+# tests start here
 echo "Test case 1: playfair cipher"
-echo "./cipher PLF chris ENC sample_input_file.txt sample_output_file.txt"
-./cipher PLF chris ENC sample_input_file.txt sample_output_file.txt
+echo "./cipher PLF chris ENC short_input_file.txt ciphertext.txt"
+./cipher PLF chris ENC short_input_file.txt ciphertext.txt
+
+echo "short input file:"
+cat short_input_file.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
 
 echo -e "\n"
 echo "Test case 2: playfair cipher"
-echo "./cipher PLF chris DEC sample_input_file.txt sample_output_file.txt"
-./cipher PLF chris DEC sample_input_file.txt sample_output_file.txt
+echo "./cipher PLF chris DEC ciphertext.txt plaintext.txt"
+./cipher PLF chris DEC ciphertext.txt plaintext.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo "decrypted ciphertext"
+cat plaintext.txt
+
+rm ciphertext.txt plaintext.txt
 
 echo -e "\n"
-echo "Test case 3: vigenere cipher"
-echo "./cipher VIG chris ENC sample_input_file.txt sample_output_file.txt"
-./cipher VIG chris ENC sample_input_file.txt sample_output_file.txt
+echo "Test case 3: row transposition cipher"
+echo "./cipher RTS chris short_input_file.txt ciphertext.txt"
+./cipher RTS chris ENC short_input_file.txt ciphertext.txt
+
+echo "short input file:"
+cat short_input_file.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
 
 echo -e "\n"
-echo "Test case 4: vigenere cipher"
-echo "./cipher VIG chris DEC sample_input_file.txt sample_output_file.txt"
-./cipher VIG chris DEC sample_input_file.txt sample_output_file.txt
+echo "Test case 4: row transposition cipher"
+echo "./cipher RTS chris DEC ciphertext.txt plaintext.txt"
+./cipher RTS chris DEC ciphertext.txt plaintext.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo "decrypted ciphertext"
+cat plaintext.txt
+
+rm ciphertext.txt plaintext.txt
 
 echo -e "\n"
-echo "Test case 5: caesar cipher"
-echo "./cipher CES 3 ENC sample_input_file.txt sample_output_file.txt"
-./cipher CES 3 ENC sample_input_file.txt sample_output_file.txt
+echo "Test case 5: railfence cipher"
+echo "./cipher RFC 3 short_input_file.txt ciphertext.txt"
+./cipher RFC 3 ENC short_input_file.txt ciphertext.txt
+
+echo "short input file:"
+cat short_input_file.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
 
 echo -e "\n"
-echo "Test case 6: casesar cipher"
-echo "./cipher CES 3 DEC sample_input_file.txt sample_output_file.txt"
-./cipher CES 3 DEC sample_input_file.txt sample_output_file.txt
+echo "Test case 6: railfence cipher"
+echo "./cipher RFC 3 DEC ciphertext.txt plaintext.txt"
+./cipher RFC 3 DEC ciphertext.txt plaintext.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo "decrypted ciphertext"
+cat plaintext.txt
+
+rm ciphertext.txt plaintext.txt
 
 echo -e "\n"
-echo "Test case 7: invalid number of command line arguments"
+echo "Test case 7: vigenere cipher"
+echo "./cipher VIG kevin ENC short_input_file.txt ciphertext.txt"
+./cipher VIG kevin ENC short_input_file.txt ciphertext.txt
+
+echo "short input file:"
+cat short_input_file.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo -e "\n"
+echo "Test case 8: vigenere cipher"
+echo "./cipher VIG kevin DEC ciphertext.txt plaintext.txt"
+./cipher VIG kevin DEC ciphertext.txt plaintext.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo "decrypted ciphertext"
+cat plaintext.txt
+
+rm ciphertext.txt plaintext.txt
+
+echo -e "\n"
+echo "Test case 9: caesaw cipher"
+echo "./cipher CES 3 ENC short_input_file.txt ciphertext.txt"
+./cipher VIG kevin ENC short_input_file.txt ciphertext.txt
+
+echo "short input file:"
+cat short_input_file.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo -e "\n"
+echo "Test case 10: caesar cipher"
+echo "./cipher CES 3 DEC ciphertext.txt plaintext.txt"
+./cipher CES 3 DEC ciphertext.txt plaintext.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo "decrypted ciphertext"
+cat plaintext.txt
+
+rm ciphertext.txt plaintext.txt
+
+echo -e "\n"
+echo "Test case 11: invalid number of command line arguments"
 echo "./cipher"
 ./cipher
 
 echo -e "\n"
-echo "Test case 8: invalid cipher name"
-echo "./cipher kevin chris ENC sample_input_file.txt sample_output_file.txt"
-./cipher kevin chris ENC sample_input_file.txt sample_output_file.txt
+echo "Test case 12: invalid cipher name"
+echo "./cipher kevin chris ENC sample_input_file.txt delete_me.txt"
+./cipher kevin chris ENC sample_input_file.txt delete_me.txt
 
 echo -e "\n"
-echo "Test case 9: invalid input file"
-echo "./cipher kevin chris ENC invalid_input_file.txt sample_output_file.txt"
-./cipher PLF chris ENC invalid_input_file.txt sample_output_file.txt
+echo "Test case 13: invalid input file"
+echo "./cipher kevin chris ENC invalid_input_file.txt delete_me.txt"
+./cipher PLF chris ENC invalid_input_file.txt delete_me.txt
 
+rm delete_me.txt
 
