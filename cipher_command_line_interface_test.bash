1f5d5406ef342584e1b0d874cdc0bb91763ec936
kevincheng99 <kevincheng99@csu.fullerton.edu>
2014-03-19 17:34:59 -0700

    update cipher CLI test: clean up files

--- cipher_command_line_interface_test.bash
+++ cipher_command_line_interface_test.bash
@@ -23,10 +23,8 @@ echo "./cipher PLF chris DEC ciphertext.txt plaintext.txt"
 echo "ciphertext:"
 cat ciphertext.txt
 
-echo "decrypted ciphertext"
+echo "decripted ciphertext:"
 cat plaintext.txt
-
-rm ciphertext.txt plaintext.txt
 
 echo -e "\n"
 echo "Test case 3: row transposition cipher"
@@ -47,10 +45,8 @@ echo "./cipher RTS chris DEC ciphertext.txt plaintext.txt"
 echo "ciphertext:"
 cat ciphertext.txt
 
-echo "decrypted ciphertext"
+echo "decripted ciphertext:"
 cat plaintext.txt
-
-rm ciphertext.txt plaintext.txt
 
 echo -e "\n"
 echo "Test case 5: railfence cipher"
@@ -71,10 +67,8 @@ echo "./cipher RFC 3 DEC ciphertext.txt plaintext.txt"
 echo "ciphertext:"
 cat ciphertext.txt
 
-echo "decrypted ciphertext"
+echo "decripted ciphertext:"
 cat plaintext.txt
-
-rm ciphertext.txt plaintext.txt
 
 echo -e "\n"
 echo "Test case 7: vigenere cipher"
@@ -95,10 +89,8 @@ echo "./cipher VIG kevin DEC ciphertext.txt plaintext.txt"
 echo "ciphertext:"
 cat ciphertext.txt
 
-echo "decrypted ciphertext"
+echo "decripted ciphertext:"
 cat plaintext.txt
-
-rm ciphertext.txt plaintext.txt
 
 echo -e "\n"
 echo "Test case 9: caesar cipher"
@@ -119,10 +111,8 @@ echo "./cipher CES 3 DEC ciphertext.txt plaintext.txt"
 echo "ciphertext:"
 cat ciphertext.txt
 
-echo "decrypted ciphertext"
+echo "decripted ciphertext:"
 cat plaintext.txt
-
-rm ciphertext.txt plaintext.txt
 
 echo -e "\n"
 echo "Test case 11: invalid number of command line arguments"
@@ -138,8 +128,6 @@ echo -e "\n"
 echo "Test case 13: invalid input file"
 echo "./cipher kevin chris ENC invalid_input_file.txt delete_me.txt"
 ./cipher PLF chris ENC invalid_input_file.txt delete_me.txt
-
-rm delete_me.txt
 
 echo -e "\n"
 echo "Test case 14: railfence cipher"
@@ -160,42 +148,120 @@ echo "./cipher RFC 100 DEC ciphertext.txt plaintext.txt"
 echo "ciphertext:"
 cat ciphertext.txt
 
-echo "decrypted ciphertext"
+echo "decripted ciphertext:"
 cat plaintext.txt
 
+echo -e "\n"
+echo "Test case 16: playfair cipher"
+echo "./cipher PLF chris ENC long_input_file.txt ciphertext.txt"
+./cipher PLF chris ENC long_input_file.txt ciphertext.txt
+
+echo "long input file:"
+cat long_input_file.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo -e "\n"
+echo "Test case 17: playfair cipher"
+echo "./cipher PLF chris DEC ciphertext.txt plaintext.txt"
+./cipher PLF chris DEC ciphertext.txt plaintext.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo "decripted ciphertext:"
+cat plaintext.txt
+
+echo -e "\n"
+echo "Test case 18: row transposition cipher"
+echo "./cipher RTS chris long_input_file.txt ciphertext.txt"
+./cipher RTS chris ENC long_input_file.txt ciphertext.txt
+
+echo "long input file:"
+cat long_input_file.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo -e "\n"
+echo "Test case 19: row transposition cipher"
+echo "./cipher RTS chris DEC ciphertext.txt plaintext.txt"
+./cipher RTS chris DEC ciphertext.txt plaintext.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo "decripted ciphertext:"
+cat plaintext.txt
+
+echo -e "\n"
+echo "Test case 20: railfence cipher"
+echo "./cipher RFC 3 long_input_file.txt ciphertext.txt"
+./cipher RFC 3 ENC long_input_file.txt ciphertext.txt
+
+echo "long input file:"
+cat long_input_file.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo -e "\n"
+echo "Test case 21: railfence cipher"
+echo "./cipher RFC 3 DEC ciphertext.txt plaintext.txt"
+./cipher RFC 3 DEC ciphertext.txt plaintext.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo "decripted ciphertext:"
+cat plaintext.txt
+
+echo -e "\n"
+echo "Test case 22: vigenere cipher"
+echo "./cipher VIG kevin ENC long_input_file.txt ciphertext.txt"
+./cipher VIG kevin ENC long_input_file.txt ciphertext.txt
+
+echo "long input file:"
+cat long_input_file.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo -e "\n"
+echo "Test case 23: vigenere cipher"
+echo "./cipher VIG kevin DEC ciphertext.txt plaintext.txt"
+./cipher VIG kevin DEC ciphertext.txt plaintext.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo "decripted ciphertext:"
+cat plaintext.txt
+
+echo -e "\n"
+echo "Test case 24: caesar cipher"
+echo "./cipher CES 3 ENC long_input_file.txt ciphertext.txt"
+./cipher CES 3 ENC long_input_file.txt ciphertext.txt
+
+echo "long input file:"
+cat long_input_file.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo -e "\n"
+echo "Test case 25: caesar cipher"
+echo "./cipher CES 3 DEC ciphertext.txt plaintext.txt"
+./cipher CES 3 DEC ciphertext.txt plaintext.txt
+
+echo "ciphertext:"
+cat ciphertext.txt
+
+echo "decripted ciphertext:"
+cat plaintext.txt
+
+# clean up files
 rm ciphertext.txt plaintext.txt
 rm .playfair_padding_location .rowtransposition_fillers
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
